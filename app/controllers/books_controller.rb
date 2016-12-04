c0b56d9fac62c1246a2ad75d02fb19281f058fd3
Yoshiki Nagatsuka <y.nagatsuka0524@gmail.com>
2016-12-04 17:56:39 +0900

    結果画面HTMLマージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,9 @@ class BooksController < ApplicationController
 	end
 
   def search
+      query_list = ["コスメ", "口紅"]
       books = Amazon::Ecs.item_search(
-                             'チョコ',
+                             query_list[rand(2)],
                              search_index: 'All',
                              dataType: 'script',
                              response_group: 'ItemAttributes, Images',
