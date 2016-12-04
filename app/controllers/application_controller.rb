726b3783fd1c132f9d32d34197facb7a74ed10bc
vagrant <vagrant@localhost.localdomain>
2016-12-04 16:10:51 +0900

    Redirect to error page if Amazon return 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	rescue_from Exception, with: :render_500
+
+	def render_500
+		render template: "errors/error_500", status: 500, layout: 'application'
+		
+	end
 end
